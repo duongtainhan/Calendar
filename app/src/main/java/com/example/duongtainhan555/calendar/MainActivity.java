7f24aaefa2336ccca53c577155e9fc8ae261a7e9
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-28 22:36:38 +0700

    Calendar - get time

--- app/src/main/java/com/example/duongtainhan555/calendar/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/calendar/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.duongtainhan555.calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView txtTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Init();
+        GetTime();
+    }
+
+    private void Init()
+    {
+        txtTime = findViewById(R.id.txtTime);
+    }
+    private void GetTime()
+    {
+        Calendar calendar = Calendar.getInstance();
+        txtTime.setText(calendar.getTime() + "");
     }
 }
